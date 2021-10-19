09e3576ebc4c536b22ee318e1d0cc5613e4c6310
Evgeniy Kazban <evgeny.kazban3.0@yandex.ru>
2021-10-19 15:47:52 +0300

    interface type

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,14 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Evgeniy", "Smith", (byte) 25);
         userService.saveUser("Alice", "Moore", (byte) 15);
